ba8361026a3ba39bec25eb470d57b1d97783a8bc
AlanYHung <72707899+AlanYHung@users.noreply.github.com>
2021-01-20 14:22:50 -0800

    Initial project setup

---
+++ LibraryCollections/LibraryCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
